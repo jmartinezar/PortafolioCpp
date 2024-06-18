025b4d39e0db59e4edf2a77c0ca3bbbe008a703c
jmartinezar <jmartinezar@unal.edu.co>
2024-06-18 14:18:49 -0500

    no corre, seguramente por mal uso de memoria dinámica

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "node.h"
+#include "tree.h"
+#include "include.h"
+#include "constants.h"
+
+int main(void)
+{
+  std::vector<std::vector<std::vector<double>>> data_problem{
+    {
+      {10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+	{0.1, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0}
+    }
+  };
+  
+  tree OctTree(data_problem);
+
+  initialized(OctTree, data_problem, dt);
+
+  OctTree.update(data_problem);
+
+  integration(OctTree, ti, tf, dt, data_problem);
+
+  animate_data(data_problem);
+
+  OctTree.~tree();
+  
+  return 0;
+}
